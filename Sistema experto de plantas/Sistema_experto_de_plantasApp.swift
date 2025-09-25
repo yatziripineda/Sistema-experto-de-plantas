2a2ebf9cfed1cdae48c8fd1ac2eafd781baa0687
Yatziri Pineda Cabrera <319132948@pcpuma.acatlan.unam.mx>
2025-09-25 15:21:32 -0600

    Initial Commit

---
+++ Sistema experto de plantas/Sistema_experto_de_plantasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sistema_experto_de_plantasApp.swift
+//  Sistema experto de plantas
+//
+//  Created by yatziri on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Sistema_experto_de_plantasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
